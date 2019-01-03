b841e21b4a564381ebc6749a5c0a1e98c548870a
lilei <1198072529@qq.com>
2019-01-03 15:31:08 +0800

    密码修改

--- apps/users/views.py
+++ apps/users/views.py
@@ -10,6 +10,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from random import choice
 
+from rest_framework_jwt.serializers import jwt_encode_handler, jwt_payload_handler
+
 from .serializers import UserRegSerializer, SmsSerializer
 from .models import VerifyCode
 
@@ -68,3 +70,18 @@ class UserViewSet(CreateModelMixin, viewsets.GenericViewSet):
     """
     serializer_class = UserRegSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+
+        re_dict = serializer.data
+        payload = jwt_payload_handler(user)
+        re_dict["token"] = jwt_encode_handler(payload)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(re_dict, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        return serializer.save()
